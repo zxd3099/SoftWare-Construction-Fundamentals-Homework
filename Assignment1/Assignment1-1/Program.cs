52f617dbbb5b6594d7cde6fda3d8e89cbcfcd593
xiaodong zhu <72681553+zxd3099@users.noreply.github.com>
2022-02-23 19:50:53 +0800

    Add files via upload

---
+++ Assignment1/Assignment1-1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zxd3099
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, res = 0;
+            char op;
+            Console.Write("Please input two numbers(Sample:2 3): ");
+            string input = Console.ReadLine();
+            string[] inputnum = input.Split(' ');
+            a = Double.Parse(inputnum[0]);
+            b = Double.Parse(inputnum[1]);
+            Console.Write("Please input an operator(Sample: +): ");
+            op = (char)Console.Read();
+            switch (op) {
+                case '+': res = a + b; break;
+                case '-': res = a - b; break;
+                case '*': res = a * b; break;
+                case '/':
+                    if (b == 0)
+                    {
+                        Console.WriteLine("The input is invalid!!");
+                        System.Environment.Exit(0);
+                    }
+                    else res = a / b; 
+                    break;
+            }
+            Console.WriteLine($"The calculation result is {res}");
+        }
+    }
+}
---
+++ Assignment1/Assignment1-2/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment1_2
+{
+    public partial class Form1 : Form
+    {
+        double a = 0, b = 0, res = 0;
+        string d;
+        bool c = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "") a = 0;
+            else a = Double.Parse(textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (d)
+            {
+                case "+":
+                    res = a + b;
+                    break;
+                case "-":
+                    res = a - b;
+                    break;
+                case "*":
+                    res = a * b;
+                    break;
+                case "/":
+                    if (b != 0) res = a / b;
+                    break;
+            }
+            textBox3.Text = res.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            d = "+";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            d = "-";
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "") b = 0;
+            else b = Double.Parse(textBox2.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            d = "*";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (b != 0)
+            {
+                d = "/";
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
