e08ddf1d3bdd24100d38e8c5fdc9c862b4f95d3e
Patric <zxd3099@163.com>
2022-03-09 18:33:46 +0800

    第四周作业

---
+++ Assignment4/AlarmClock/AlarmClock.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_2
+{
+    class ClockTest
+    {
+        public void Alarm(object sender, DateTime time)
+        {
+            Console.WriteLine($"The set time {time} has come!");
+        }
+
+        public void Tick(object sender, DateTime time)
+        {
+            Console.WriteLine($"Now is {time}");
+        }
+        static void Main(string[] args)
+        {
+            AlarmClock clock = new AlarmClock();
+            ClockTest clockTest = new ClockTest();
+            DateTime time = DateTime.Now.AddSeconds(5);
+            clock.AlarmTime = time;
+
+            clock.OnAlarm += clockTest.Alarm;
+            clock.OnTick += clockTest.Tick;
+
+            clock.StartClock();
+        }
+    }
+
+    class AlarmClock
+    {
+        private DateTime alarmTime = DateTime.Now;
+
+        public DateTime AlarmTime {
+            get => alarmTime;
+            set => alarmTime = value;
+        }
+
+        public delegate void TickHandler(object sender, DateTime args);
+        public delegate void AlarmHandler(object sender, DateTime args);
+
+        public event AlarmHandler OnAlarm;
+        public event TickHandler OnTick;
+
+        public void StartClock()
+        {
+            while (true)
+            {
+                DateTime presentTime = DateTime.Now;
+                OnTick(this, presentTime);
+                if (presentTime.ToString() == alarmTime.ToString())
+                {
+                    OnAlarm(this, alarmTime);
+                }
+                // every one second excute
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Assignment4/GenericList/GenericList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_1
+{
+    class GenericListTest
+    {
+        static void Main(string[] args)
+        {
+            // Assign a random number to each node of the linked list
+            int sum = 0;
+            Random r = new Random();
+            GenericList<int> list = new GenericList<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                list.Add(r.Next(1, 100));
+            }
+            int maxv = -1, minv = 105;
+            list.ForEach(m => Console.WriteLine(m));
+            list.ForEach(m => maxv = Math.Max(m, maxv));
+            list.ForEach(m => minv = Math.Min(m, minv));
+            list.ForEach(m => sum += m);
+            Console.WriteLine($"Maximum value is {maxv}");
+            Console.WriteLine($"Minimum value is {minv}");
+            Console.WriteLine($"The sum is {sum}");
+
+        }
+    }
+    
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            head = tail = null;
+        }
+        public Node<T> Head { get => head; }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+
+        }
+        public void ForEach(Action<T> action)
+        {
+            for(Node<T> m = head; m.Next != null; m = m.Next)
+            {
+                action(m.Data);
+            }
+        }
+    }
+}
